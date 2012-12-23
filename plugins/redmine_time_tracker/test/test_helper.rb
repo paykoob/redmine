324346ede06e5bf5b4919cde1e1b8bd90bf77af6
Hossein Ansari <hossein.ansari@gmail.com>
2012-12-23 10:48:58 +0330

    remove time_tracker

--- plugins/redmine_time_tracker/test/test_helper.rb
+++
@@ -1,7 +0,0 @@
-# Load the Redmine helper
-require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
-ActiveSupport::TestCase.fixture_path=File.dirname(__FILE__) + '/fixtures/'
-
-# next two lines are necessary to get Unit-Tests handled by RubyMine
-require "minitest/reporters"
-MiniTest::Reporters.use!
